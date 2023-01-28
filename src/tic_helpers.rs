0f99590c6d53886eb05d08e0c704a628630b2cef
Adam <evilspamalt@gmail.com>
2023-01-28 23:49:18 +1000

    Use named VRAM pointers, reimplement poke4

--- src/tic_helpers.rs
+++ src/tic_helpers.rs
@@ -1,58 +1,85 @@
 use crate::tic80::*;
 
+/// Ported straight from TIC80's poke4 function.
+/// I hope this is safe.
+unsafe fn rust_poke4(index: i32, value: u8) {
+    // #define POKE_N(P,I,V,A,B,C,D) do        \
+    // {                                       \
+    //     u8* val = (u8*)(P) + ((I) >> (A));  \
+    //     u8 offset = ((I) & (B)) << (C);     \
+    //     *val &= ~((D) << offset);           \
+    //     *val |= ((V) & (D)) << offset;      \
+    // } while(0)
+    /*#define PEEK_N(P,I,A,B,C,D) ( ( ((u8*)(P))[((I) >> (A))] >> ( ((I) & (B)) << (C) ) ) & (D) )
+    
+    inline void tic_tool_poke4(void* addr, u32 index, u8 value)
+    {
+        POKE_N(addr, index, value, 1,1,2,15);
+    }*/
+    
+    // Clamp to TIC80 reserved RAM.
+    let index = index.clamp(0, 98322*2);
+    let val: *mut u8 = (index as usize >> 1) as *mut u8;
+    let offset: u8 = (((index) & (1)) as u8) << (2);
+    unsafe {
+        *val &= !((15) << offset);
+        *val |= ((value) & (15)) << offset;
+    }
+}
+
 pub fn palette_map_swap(from: u8, to: u8) {
     let from: i32 = (from % 16).into();
     assert!(from >= 0);
-    unsafe { poke4(0x3FF0 * 2 + from, to % 16) }
+    unsafe { rust_poke4(PALETTE_MAP as i32 * 2 + from, to % 16) }
 }
 
 pub fn palette_map_set_all(to: u8) {
     for i in 0..=15 {
-        unsafe { poke4(0x3FF0 * 2 + i, to % 16) }
+        unsafe { rust_poke4(PALETTE_MAP as i32 * 2 + i, to % 16) }
     }
 }
 
 pub fn set_palette_map(map: [u8; 16]) {
     for (i, item) in map.into_iter().enumerate() {
-        unsafe { poke4(0x3FF0 * 2 + i as i32, item % 16) }
+        unsafe { rust_poke4(PALETTE_MAP as i32 * 2 + i as i32, item % 16) }
     }
 }
 
 pub fn palette_map_reset() {
     for i in 0..=15 {
-        unsafe { poke4(0x3FF0 * 2 + i, i as u8) }
+        unsafe { rust_poke4(PALETTE_MAP as i32 * 2 + i, i as u8) }
     }
 }
 
 pub fn palette_map_rotate(amount: u8) {
     for i in 0..=15 {
-        unsafe { poke4(0x3FF0 * 2 + i, i as u8 + amount) }
+        unsafe { rust_poke4(PALETTE_MAP as i32 * 2 + i, i as u8 + amount) }
     }
 }
 
 pub fn get_palette_map() -> [u8; 16] {
     let mut palette_map = [0; 16];
     for (i, x) in palette_map.iter_mut().enumerate() {
-        unsafe { *x = peek4(0x3FF0 * 2 + i as i32) }
+        unsafe { *x = peek4(PALETTE_MAP as i32 * 2 + i as i32) }
     }
     palette_map
 }
 
 pub fn palette(index: u8, rgb: [u8; 3]) {
-    let index: i32 = (index % 16).into();
+    let index: usize = (index % 16).into();
     for (i, colour) in rgb.into_iter().enumerate() {
-        unsafe { poke(0x3FC0 + index + i as i32, colour) }
+        unsafe { (*PALETTE)[index + i] = colour}
     }
 }
 
 pub fn set_border(colour: u8) {
-    unsafe { poke(0x3FF8, colour) }
+    unsafe { *BORDER_COLOR = colour }
 }
 
 pub fn screen_offset(horizontal: u8, vertical: u8) {
     unsafe {
-        poke(0x3FF9, horizontal);
-        poke(0x3FFA, vertical);
+        (*SCREEN_OFFSET)[0] = horizontal;
+        (*SCREEN_OFFSET)[1] = vertical;
     }
 }
 
@@ -89,22 +116,6 @@ pub fn spr_outline(id: i32, x: i32, y: i32, sprite_options: SpriteOptions, outli
     set_palette_map(old_map);
     spr(id, x, y, sprite_options);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 pub const SWEETIE_16: [[u8; 3]; 16] = [
     [26, 28, 44],// #1a1c2c
