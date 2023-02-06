76b8c5dd7e8f10a47cf0a47ff9f59f0024f7ec7d
Adam <evilspamalt@gmail.com>
2023-02-07 00:00:45 +1000

    Add living room map

--- src/map_data.rs
+++ src/map_data.rs
@@ -403,7 +403,7 @@ pub static HOUSE_STAIRWELL: MapSet<'static> = MapSet {
         },
     ],
     warps: &[Warp::new(Hitbox::new(1,3*8,8,8),Some(&BEDROOM),Vec2::new(16*8,5*8)),
-             Warp::new(Hitbox::new(7*8,9*8,2*8,8),Some(&SUPERMARKET),Vec2::new(10*8,7*8))],
+             Warp::new(Hitbox::new(7*8,9*8,2*8,8),Some(&HOUSE_LIVING_ROOM),Vec2::new(10*8,4*8))],
     interactables: &[
         Interactable {
             hitbox: Hitbox::new(2*8, 2*8, 3*8, 8),
@@ -419,6 +419,82 @@ pub static HOUSE_STAIRWELL: MapSet<'static> = MapSet {
             hitbox: Hitbox::new(15*8, 3*8, 8, 8),
             interaction: Interaction::Text(HOUSE_STAIRWELL_DOOR),
             sprite: None,
+        },
+    ],
+    ..DEFAULT_MAP_SET
+};
+
+pub static HOUSE_LIVING_ROOM: MapSet<'static> = MapSet {
+    maps: &[
+        MapOptions { //room
+            x: 67,
+            y: 0,
+            w: 23,
+            h: 13,
+            transparent: &[],
+            ..DEFAULT_MAP
+        },
+        MapOptions { //couch
+            x: 37,
+            y: 13,
+            w: 4,
+            h: 3,
+            transparent: &[0],
+            sx: 12*8,
+            sy: 7*8,
+            ..DEFAULT_MAP
+        },
+        MapOptions { //tv
+            x: 41,
+            y: 13,
+            w: 2,
+            h: 3,
+            transparent: &[0],
+            sx: 15*8,
+            sy: 9*8-1,
+            ..DEFAULT_MAP
+        },
+    ],
+    warps: &[Warp::new(Hitbox::new(10*8,4*8,2*8,8),Some(&HOUSE_STAIRWELL),Vec2::new(7*8,7*8)),
+    Warp::new(Hitbox::new(3*8,9*8,8,8),Some(&SUPERMARKET),Vec2::new(14*8,5*8))],
+    interactables: &[
+        Interactable {
+            hitbox: Hitbox::new(12*8, 7*8, 3*8, 4*8),
+            interaction: Interaction::Text(HOUSE_LIVING_ROOM_COUCH),
+            sprite: None,
+        },
+        Interactable {
+            hitbox: Hitbox::new(15*8, 9*8-1, 2*8, 3*8),
+            interaction: Interaction::Text(HOUSE_LIVING_ROOM_TV),
+            sprite: None,
+        },
+        Interactable {
+            hitbox: Hitbox::new(5*8, 7*8, 8, 8),
+            interaction: Interaction::Text(HOUSE_LIVING_ROOM_WINDOW),
+            sprite: None,
+        },
+        Interactable {
+            hitbox: Hitbox::new(8*8, 6*8, 8, 8),
+            interaction: Interaction::Text(CONSTRUCTION_2),
+            sprite: None,
+        },
+        Interactable {
+            hitbox: Hitbox::new(14*8, 6*8, 8, 8),
+            interaction: Interaction::Text(CONSTRUCTION_2),
+            sprite: None,
+        },
+        Interactable {
+            hitbox: Hitbox::new(12*8+7, 7*8, 8, 8),
+            interaction: Interaction::Text(HOUSE_LIVING_ROOM_COUCH),
+            sprite: Some(Animation {
+                frames: &[
+                    AnimFrame::new(Vec2::new(0, 0), 576, 30, SpriteOptions {w: 2, h: 3,
+                        ..SpriteOptions::transparent_zero()}),
+                    AnimFrame::new(Vec2::new(0, 0), 578, 30, SpriteOptions {w: 2, h: 3,
+                        ..SpriteOptions::transparent_zero()}),
+                ],
+                ..Animation::const_default()
+            }),
         },
     ],
     ..DEFAULT_MAP_SET
